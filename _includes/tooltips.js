21591daf4b50bbc9f871d4c6fcb9f182c8d5ce8e
David Laing <dlaing@imbellus.com>
2020-05-12 00:57:49 -0700

    saving work for the night

---
+++ _includes/tooltips.js
@@ -0,0 +1,5 @@
+tippy('#example2', {
+    theme: 'light',
+    content: 'Lorem ipsum <a id="example2" href="/example2">internal link</a> sit [about](/about), [wikipeida](wikipedia.org) consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna condimentum mattis pellentesque id. At in tellus integer feugiat scelerisque varius morbi. Pretium nibh ipsum consequat nisl vel pretium lectus quam. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Volutpat commodo sed egestas egestas fringilla phasellus',
+    allowHTML: true
+});
